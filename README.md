c3f6dbb9d7a07acb601a86d7cd6721d5f39855f2
lfrojdh <lars.frojdh@gmail.com>
2022-09-06 15:38:20 +0200

    improved argument handling

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # zm
 Zero Make - make done easy
+
+zm mimics make by storing buildrules and builds a simple dependency graph.
+
+A simple example:
+
+```bash
+echo -e "int sum(int a, int b)\n{\n\treturn a+b;\n}" > foo.c
+
+echo -e "#include<stdio.h>\n\nextern int sum(int a, int b);\n\nint main(void)\n{\n\tprintf(\"sum %i\", sum(1,2));\n}\n" > bar.c
+
+zm -c bar.c
+zm -c foo.c
+zm bar.o foo.o
+```
+
+change any file and rebuild a.out by:
+
+```bash
+zm
+```
+
+generate corresponding makefile by:
+
+```bash
+zm --make
+```
+
+```make
+bar.o:	bar.c
+	gcc -c bar.c
+
+foo.o:	foo.c
+	gcc -c foo.c
+
+a.out:	bar.o foo.o
+	gcc bar.o foo.o
+```
+
+To remove object files:
+
+```bash
+zm --clean
+```
+
+Purge database (.zm.pickle):
+
+```bash
+zm --purge
+```
--- zm.py
+++ zm.py
@@ -2,65 +2,119 @@
 
 import os
 import sys
-import re
+import argparse
 import pickle
 import subprocess
 
+
+def build(db, target, components):
+    if target and components:
+        if any(
+            map(
+                lambda component: os.path.isfile(component)
+                and os.path.isfile(target)
+                and os.stat(component).st_mtime > os.stat(target).st_mtime
+                or not os.path.isfile(target),
+                components.split(" "),
+            )
+        ):
+
+            run(db[(target, components)])
+
+
+def run(cmd):
+    print("zm: " + cmd)
+    return subprocess.run(cmd, shell=True).returncode == 0
+
+
 if __name__ == "__main__":
 
-    db = {}
+    db = {}  # [("target", ["component"])] = "buildrule"
 
     CC = os.getenv("CC") and os.getenv("CC") or "gcc"
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--clean", help="remove object files", action="store_true")
+    parser.add_argument("--build", help="build", action="store_true")
+    parser.add_argument("--purge", help="removed all stored data", action="store_true")
+    parser.add_argument(
+        "--make", help="generate template makefile", action="store_true"
+    )
+
+    args, unknown = parser.parse_known_args()
+
+    args.build = args.build or len(sys.argv) == 1
+
     if os.path.isfile(".zm.pickle"):
         db = pickle.load(open(".zm.pickle", "rb"))
-    elif len(sys.argv) == 1:
-        print("zero make - make done easy")
-        print("usage=zm.py [options] and CC build commands")
-        print("--clean\tremove object files")
-        print("--rebuild\trebuild everything")
-        print("--purge\tremove all stored data")
-        print("--make\tgenerate template makefile")
-        print("--list\tlist template makefile")
-        sys.exit(0)
-        
-    if len(sys.argv) == 1:
-        for k in db:
-            (cfile, obj) = k
-            if not os.path.isfile(obj) or not os.path.isfile(cfile) or os.stat(cfile).st_mtime > os.stat(obj).st_mtime:
-                subprocess.run(CC + " " + " ".join(db[k]), shell=True)
 
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '--clean':
-            for k in db:
+    if args.clean:
+        for (target, component) in db:
+            if target:
+                print("rm " + target)
                 try:
-                    os.remove(k[1])
-                    print("rm " + k[1])
-                except: 
+                    os.remove(target)
+                except:
                     pass
+        sys.exit(0)
 
-        elif sys.argv[1] == '--purge':
-            db = {}
-            os.remove(".zm.pickle")
+    if args.purge:
+        db = {}
+        print("rm .zm.pickle")
+        os.remove(".zm.pickle")
+        sys.exit(0)
 
-        elif sys.argv[1] == '--list':
-            for k in db:
-                print("CC " + " ".join(db[k]))                
-            
-        elif sys.argv[1] == '--rebuild':
-            pass
-        else:
-            l = list(filter(lambda x: x.endswith('.c') and os.path.isfile(x), sys.argv[1:]))
+    if args.make:
+        for (component, target) in db:
+            print(component + ":\t" + target)
+            print("\t" + db[(component, target)])
+            print("")
+        sys.exit(0)
 
-            # found c-file argslist            
-            if len(l):
+    if args.build:
 
-                # only add rules which exit != 0
-                r = subprocess.run(CC + " " + " ".join(sys.argv[1:]), shell=True)
-                if r.returncode:
-                    sys.exit(r.returncode)
+        if not len(db):
+            print("zm: no build rules added")
 
-                db[(l[0],l[0].replace(".c", ".o"))] = sys.argv[1:]
+        for (target, components) in db:
+            build(db, target, components)
+
+        sys.exit(0)
+
+    # default execute and store buildrule
+    if not run(CC + " " + " ".join(sys.argv[1:])):
+        sys.exit(1)
+
+    component = ""
+    target = ""
+
+    try:
+        i = sys.argv.index("-c")
+        if os.path.isfile(sys.argv[i + 1]):
+            component = sys.argv[i + 1]
+    except:
+        pass
+
+    try:
+        i = sys.argv.index("-o")
+        if os.path.isfile(sys.argv[i + 1]):
+            target = sys.argv[i + 1]
+    except:
+        pass
+
+    # implicit target
+    if component and not target and component.endswith(".c"):
+        target = component.replace(".c", ".o")
+
+    # implicit a.out target (link target)
+    if not component and not target:
+        component = " ".join(
+            list(filter(lambda x: x.endswith(".o") and os.path.isfile(x), sys.argv[1:]))
+        )
+        target = "a.out"
+
+    db[(target, component)] = " ".join([CC] + sys.argv[1:])
 
     if db:
         pickle.dump(db, open(".zm.pickle", "wb"))
